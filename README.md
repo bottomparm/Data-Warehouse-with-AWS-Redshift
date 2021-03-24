db53ba24058ae52c01108059f010196f3cda1093
Dylan Hrebenach <dhrebenach@underdogmedia.com>
2021-03-24 08:52:19 -0700

    Updating readme, removing unused dwh.cfg params.

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # README
+
+## Installation
+
+Use a package manager (pip) to install the following packages:
+
+```bash
+pip install configparser
+pip install psycopg2
+pip install psycopg2-binary
+pip install pandas
+pip install boto3
+```
+
+## Usage
+
+1. You'll need to create a dwh.cfg file with the following information:
+
+```config
+[AWS]
+KEY=''
+SECRET=''
+
+[CLUSTER]
+DB_NAME=''
+DB_USER=''
+DB_PASSWORD=''
+DB_PORT=''
+CLUSTER_IDENTIFIER=''
+CLUSTER_TYPE=''
+NUM_NODES=''
+NODE_TYPE=''
+
+[IAM_ROLE]
+NAME=''
+
+[S3]
+LOG_DATA='s3://udacity-dend/log_data'
+LOG_JSONPATH='s3://udacity-dend/log_json_path.json'
+SONG_DATA='s3://udacity-dend/song_data'
+```
+
+2. Run the create_tables.py file. NOTE: You'll want to do this once, and wait 5-10 minutes for your Redshift Cluster to be created before running again.
+
+3. 
--- create_tables.py
+++ create_tables.py
@@ -6,15 +6,9 @@ import awsConfig
 config = configparser.ConfigParser()
 config.read("dwh.cfg")
 
-DWH_IAM_ROLE_NAME = config["IAM_ROLE"]["NAME"]
 DWH_DB = config["CLUSTER"]["DB_NAME"]
 DWH_DB_USER = config["CLUSTER"]["DB_USER"]
 DWH_DB_PASSWORD = config["CLUSTER"]["DB_PASSWORD"]
-DWH_CLUSTER_IDENTIFIER = config["CLUSTER"]["CLUSTER_IDENTIFIER"]
-DWH_CLUSTER_TYPE = config["CLUSTER"]["CLUSTER_TYPE"]
-DWH_NUM_NODES = config["CLUSTER"]["NUM_NODES"]
-DWH_NODE_TYPE = config["CLUSTER"]["NODE_TYPE"]
-DWH_PORT = config["CLUSTER"]["DB_PORT"]
 
 def drop_tables(cur, conn):
     try:
@@ -35,10 +29,12 @@ def create_tables(cur, conn):
 
 
 def main():
+    # run the awsConfig main func to create AWS resources and receive cluster endpoint
+    # NOTE: you will have to run this multiple times in order to give time for the cluster to create
     endpoint = awsConfig.main()
     host = endpoint['Address']
     port = endpoint['Port']
-
+    # connect to postgres db
     conn = psycopg2.connect(
         "host={} dbname={} user={} password={} port={}".format(
             host, DWH_DB, DWH_DB_USER, DWH_DB_PASSWORD, port
